4409f034a628233576004951ec1cf4362d540588
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2022-10-19 14:42:21 +0530

    Add files via upload
    *
    **
    ***
    ****

---
+++ exercise/pattern/increment.py
@@ -0,0 +1,10 @@
+#U - > D incremenet
+n = 5
+i = 1
+while i <= n:
+    j = 1  
+    while j <= i:
+        print(j,end="")
+        j = j + 1
+    print()
+    i = i + 1
---
+++ exercise/pattern/increment_step.py
@@ -0,0 +1,9 @@
+n = 5
+i = 1
+while i <= n:
+    j = 1  
+    while j <= i:
+        print(i*2-1,end="")
+        j = j + 1
+    print()
+    i = i + 1
