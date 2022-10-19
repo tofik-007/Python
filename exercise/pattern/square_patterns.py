95d33e2d46563074ddda2a0e79980323c4a6b9ed
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2022-10-19 14:39:40 +0530

    Add files via upload
    1111
    1111
    1111
    1111

---
+++ exercise/pattern/square_patterns.py
@@ -0,0 +1,52 @@
+n = 5
+print("*")
+i = 1
+while i <= n:
+    j = 1
+    while j <= n:
+        print("*",end="")
+        j = j + 1
+    print()
+    i = i + 1 
+print()
+print("1111\n")
+i = 1
+while i <= n:
+    j = 1  
+    while j <= n:
+        print(i,end="")
+        j = j + 1
+    print()
+    i = i + 1
+print()
+print("1234\n")
+i = 1
+while i <= n:
+    j = 1  
+    while j <= n:
+        print(j,end="")
+        j = j + 1
+    print()
+    i = i + 1
+print()
+print("3210\n")
+i = 1
+while i <= n:
+    j = 1  
+    while j <= n:
+        print(n-j,end="")
+        j = j + 1
+    print()
+    i = i + 1
+print()
+print("4321\n")
+i = 1
+while i <= n:
+    j = 1  
+    while j <= n:
+        print(n-j+1,end="")
+        j = j + 1
+    print()
+    i = i + 1
+
+    
